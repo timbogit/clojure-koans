3fc5151ba9ec3d905a82a6044bef9c44c00b1673
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-12-19 20:31:10 -0700

    up to 09_runtime_polymorphism.clj

--- src/koans/08_higher_order_functions.clj
+++ src/koans/08_higher_order_functions.clj
@@ -27,9 +27,9 @@
   (= 24 (reduce (fn [a b] (* a b)) [1 2 3 4]))
 
   "You can start somewhere else"
-  (= 2400 (reduce (fn [a b] (* a b)) __ [1 2 3 4]))
+  (= 2400 (reduce (fn [a b] (* a b)) 100 [1 2 3 4]))
 
   "Numbers are not the only things one can reduce"
   (= "longest" (reduce (fn [a b]
-                         (if (< __ __) b a))
+                         (if (< (count a) (count b)) b a))
                        ["which" "word" "is" "longest"])))
